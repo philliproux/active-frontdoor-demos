51096bb6f728f5e18548b054ef3512ddd5fda0f1
Phillip Roux <phillip.roux@outlook.com>
2019-08-13 10:50:06 +0200

    Demo - Infra as code - Azure CLI

--- frontdoor-affinity-web-app-affinity/DeployInfra.ps1
+++ frontdoor-affinity-web-app-affinity/DeployInfra.ps1
@@ -1,9 +1,3 @@
-# TODO
-# - FD: HTTP and HTTPS - done, Caching - done, Session Affinity
-# - Web Apps: Env Variables
-# - Search usage of each param
-# - SP
-# - Incorporate into YAML Pipeline
 param (
     [Parameter(Mandatory)]
     [string]$frontDoorResourceGroup,
@@ -36,19 +30,13 @@ param (
 )
 
 <#
-    -- MOVE INTO UNIFIED PS FILE -- 
-    - Az Login
-    - Clone Fika App
-    - Provision Infra
-    - Build Solution / Publish to Azure to both blue/green.. http://tattoocoder.com/dotnet-azure-a-net-core-global-tool-to-deploy-an-application-to-azure-in-one-command/
-    - Toggle
-    .\DeployInfra.ps1 -frontDoorResourceGroup "PR-frontdoor-bluegreen-rg1" -frontDoorName "frontdoor-blue-green-pr1" -frontDoorBackendPoolName "DefaultBackendPool" -webAppBlue "frontdoor-web-blue3" -webAppGreen "frontdoor-web-green3" -webAppBlueUrl "frontdoor-web-blue3.azurewebsites.net" -webAppGreenUrl "frontdoor-web-green2.azurewebsites.net" -appServicePlanBlue "appservice-blue3" -appServicePlanGreen "appservice-green3" -appServicePlanSize "B1"
-    .\DeployInfra.ps1 -frontDoorResourceGroup "pr-frontdoor-bluegreen-rg1" -frontDoorName "frontdoor-blue-green-pr1" -frontDoorBackendPoolName "DefaultBackendPool" -webAppBlue "frontdoor-web-blue-pr1" -webAppGreen "frontdoor-web-green-pr1" -webAppBlueUrl "frontdoor-web-blue-pr1.azurewebsites.net" -webAppGreenUrl "frontdoor-web-green-pr1.azurewebsites.net" -appServicePlanBlue "appservice-blue-pr1" -appServicePlanGreen "appservice-green3" -appServicePlanSize "D1"
-    .\ToggleFrontdoorBackends.ps1 -frontDoorResourceGroup "frontdoor-bluegreen-rg2" -frontDoorName "frontdoor-blue-green2" -frontDoorBackendPoolName "DefaultBackendPool" -frontDoorUrl "http://frontdoor-blue-green2.azurefd.net" -webAppBlueUrl "frontdoor-web-blue2.azurewebsites.net" -webAppGreenUrl "frontdoor-web-green2.azurewebsites.net"
+    - .Commit Web App into repo
+    - .Test
+    - New App without session affinity
 #>
 
 Write-Host "Delete Resource Group: " $frontDoorResourceGroup
-az group delete --name $frontDoorResourceGroup
+#az group delete --name $frontDoorResourceGroup
 
 # Instantiate variables
 $frontDoorFQDN = $frontDoorName + ".azurefd.net"
@@ -68,6 +56,10 @@ $frontDoorRouteName = "DefaultRoutingRule" # Default value created by FD
 $frontDoorRouteAcceptedProtocols = "Https"
 $frontDoorRouteCaching = "Disabled"
 $frontDoorEndPointName = "DefaultFrontendEndpoint" # Default value created by FD
+$frontDoorSessionAffinityEnabled = "Enabled"
+
+$webAppNumberOfWorkers = 2
+$gitrepo = "https://github.com/philliproux/fika-frontdoor-webapp.git"
 
 Write-Host "Front Door Hostname:" $frontDoorFQDN
 
@@ -76,12 +68,12 @@ Write-Host "Create Resource Group"
 az group create -l westeurope -n $frontDoorResourceGroup
 
 Write-Host "Create Blue Web App"
-az appservice plan create -g $frontDoorResourceGroup -n $appServicePlanBlue --sku $appServicePlanSize
-az webapp create -g $frontDoorResourceGroup -p $appServicePlanBlue -n $webAppBlue
+az appservice plan create -g $frontDoorResourceGroup -n $appServicePlanBlue --sku $appServicePlanSize --number-of-workers $webAppNumberOfWorkers
+az webapp create -g $frontDoorResourceGroup -p $appServicePlanBlue -n $webAppBlue 
 
 Write-Host "Create Green Web App"
-az appservice plan create -g $frontDoorResourceGroup -n $appServicePlanGreen
-az webapp create -g $frontDoorResourceGroup -p $appServicePlanGreen -n $webAppGreen
+az appservice plan create -g $frontDoorResourceGroup -n $appServicePlanGreen --sku $appServicePlanSize --number-of-workers $webAppNumberOfWorkers
+az webapp create -g $frontDoorResourceGroup -p $appServicePlanGreen -n $webAppGreen 
 
 Write-Host "Create Front Door"
 az network front-door create --backend-address $frontDoorFQDN --name $frontDoorName --resource-group $frontDoorResourceGroup
@@ -96,11 +88,13 @@ az network front-door load-balancing create --front-door-name $frontDoorName --n
 Write-Host "Create Health Probe"
 az network front-door probe create --front-door-name $frontDoorName --interval $frontDoorHealthProbeIntervalInSeconds --name $frontDoorHealthProbeName --path $frontDoorHealthProbePath --resource-group $frontDoorResourceGroup --protocol $frontDoorHealthProbeProtocol  #{Http, Https}
 
-Write-Host "Create Backend Pool"
+Write-Host "Create Backend Pool with Green Backend"
 az network front-door backend-pool create --address $webAppGreenUrl --front-door-name $frontDoorName --load-balancing $frontDoorLoadBalancingName --name $frontDoorBackEndPoolName --probe $frontDoorHealthProbeName --resource-group $frontDoorResourceGroup
+Write-Host "Add blue backend to backend pool"
+az network front-door backend-pool backend add --address $webAppBlueUrl --front-door-name $frontDoorName --pool-name $frontDoorBackEndPoolName --resource-group $frontDoorResourceGroup
 
 Write-Host "Create Front Door Route Rule"
-az network front-door routing-rule create --front-door-name $frontDoorName --frontend-endpoint $frontDoorEndPointName  --name $frontDoorRouteName --resource-group $frontDoorResourceGroup --route-type $frontDoorRouteType --backend-pool $frontDoorBackEndPoolName --accepted-protocols $frontDoorRouteAcceptedProtocols --caching $frontDoorRouteCaching #--patterns "/api/*"
+az network front-door routing-rule create --front-door-name $frontDoorName --frontend-endpoint $frontDoorEndPointName --name $frontDoorRouteName --resource-group $frontDoorResourceGroup --route-type $frontDoorRouteType --backend-pool $frontDoorBackEndPoolName --accepted-protocols $frontDoorRouteAcceptedProtocols --caching $frontDoorRouteCaching  #--patterns "/api/*"
 
 # List Front Door Resources
 Write-Host "`nList Backend Pools"
@@ -122,3 +116,14 @@ Write-Host "`nList FrontEnd Endpoints"
 az network front-door frontend-endpoint list --front-door-name $frontDoorName --resource-group $frontDoorResourceGroup -o table
 
 Write-Host "`nFront Door Hostname:" $frontDoorFQDN
+
+# Set Web App Settings
+az webapp config appsettings set -g $frontDoorResourceGroup -n $webAppBlue --settings DEPLOYMENT_HOST_COLOR="BLUE"
+az webapp config appsettings set -g $frontDoorResourceGroup -n $webAppBlue --settings ENVIRONMENT_NAME="ENV_GREEN"
+
+az webapp config appsettings set -g $frontDoorResourceGroup -n $webAppGreen --settings DEPLOYMENT_HOST_COLOR="GREEN"
+az webapp config appsettings set -g $frontDoorResourceGroup -n $webAppGreen --settings ENVIRONMENT_NAME="ENV_GREEN"
+
+# Deploy Web App from Github
+az webapp deployment source config --name $webAppBlue --resource-group $frontDoorResourceGroup --repo-url $gitrepo --branch master --manual-integration
+az webapp deployment source config --name $webAppGreen --resource-group $frontDoorResourceGroup --repo-url $gitrepo --branch master --manual-integration
--- frontdoor-affinity-web-app-affinity/az-login.ps1
+++ frontdoor-affinity-web-app-affinity/az-login.ps1
@@ -1,2 +1,12 @@
+# Login to Azure
+
+#az login 
+
 # Show subscription list
-az account list --output table
+# az account list --output table
+
+# # Set Subscription
+# az account set --subscription "Phillip VS - ITAS"
+
+# # List resource groups
+# az group list --output table
