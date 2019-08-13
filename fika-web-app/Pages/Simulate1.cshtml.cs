c3781549121a8755a4ef0c26b0691f30db6e5615
Phillip Roux <phillip.roux@outlook.com>
2019-08-13 09:14:29 +0200

    Sample .net core web app

---
+++ fika-web-app/Pages/Simulate1.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Fika.Web.Pages
+{
+    public class SimulateModel : PageModel
+    {
+        public void OnGet()
+        {
+            HttpContext.Response.Cookies.Append("ARRAffinity", Guid.NewGuid().ToString());
+        }
+
+        public void OnGetSetAffinity()
+        {
+            
+        }
+    }
+}
--- frontdoor-affinity-web-app-affinity/az-login.ps1
+++ frontdoor-affinity-web-app-affinity/az-login.ps1
@@ -10,25 +10,3 @@
 
 # # List resource groups
 # az group list --output table
-
-az resource create -g "pr-active-frontdoor-demo-2" -n "DefaultFrontendEndpoint" --resource-type "Microsoft.Network/frontdoors/frontendendpoints" --is-full-object --properties '{
-    "identity": null,
-    "kind": null,
-    "location": null,
-    "managedBy": null,
-    "name": "DefaultFrontendEndpoint",
-    "plan": null,
-    "properties": {
-      "customHttpsConfiguration": null,
-      "customHttpsProvisioningState": "Disabled",
-      "customHttpsProvisioningSubstate": "None",
-      "hostName": "pr-active-frontdoor-demo-2.azurefd.net",
-      "resourceState": "Enabled",
-      "sessionAffinityEnabledState": "Disabled",
-      "sessionAffinityTtlSeconds": 0,
-      "webApplicationFirewallPolicyLink": null
-    },
-    "resourceGroup": "pr-active-frontdoor-demo-2",
-    "sku": null,
-    "tags": null
-}'
